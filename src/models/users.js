6ef3028915e762952e98fb16ea0ce973a3ba121d
mayangi aretha <myaretha2620@gmail.com>
2022-08-22 18:30:35 +0300

    add the users feature

---
+++ src/models/users.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  userId: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: String,
+    required: false
+  },
+});
+let users = mongoose.model('user', userSchema);
+export default users;
